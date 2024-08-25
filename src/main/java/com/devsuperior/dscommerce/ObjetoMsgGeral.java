41f3a1be54fc1264a9979f296b292e8f9bbf4188
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-25 18:16:52 -0300

    Bakend Pronto para implantação nuvem

---
+++ src/main/java/com/devsuperior/dscommerce/ObjetoMsgGeral.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscommerce;
+
+public class ObjetoMsgGeral {
+	
+	private String msg;
+	
+	public ObjetoMsgGeral(String msg) {
+		this.msg = msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+}
