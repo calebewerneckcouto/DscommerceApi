43c10a3f1193ae95691b6aad5b419954a8349e4f
calebewerneckcouto <calebewerneck@hotmail.com>
2024-10-24 20:10:39 -0300

    make it better

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ObejtoMsgGeral.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscommerce.dto;
+public class ObejtoMsgGeral {
+	
+	private String msg;
+	
+	public ObejtoMsgGeral(String msg) {
+		this.msg = msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+}
