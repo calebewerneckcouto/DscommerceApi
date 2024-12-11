c189db1c412fe12cb7fcdcdecf5cb886f2a1a44f
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-11 14:33:12 -0300

    teste busca de produto por nome

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscommerce.services;
 
+import java.util.List;
 import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,9 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -32,6 +39,8 @@ public class ProductServiceTests {
 	
 	private Product product;
 	
+	private PageImpl<Product>page;
+	
 	@BeforeEach // antes de rodar qualquer coisa, execute isso....
 	void setup()throws Exception {
 		existingId =1L;
@@ -41,8 +50,11 @@ public class ProductServiceTests {
 		
 		product = ProductFactory.createProduct(productName);
 		
-		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
-		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		page = new PageImpl<Product>(List.of(product));
+		
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));//retorna optional (exceção caso nao ache id)
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());//retorna vazio pq nao tem nada na lista
+		Mockito.when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);//retonar pagina...
 
 	}
 	
@@ -71,6 +83,19 @@ public class ProductServiceTests {
 		});
 		
 		
+		
+	}
+	
+	
+	@Test//Deve retornar pagina produto min Dto caso nao seja nulo, seja tamanho 1 (lista) e o nome seja igual o setado
+	public void findAllShouldReturnPagedProductMinDTO() {
+		
+		Pageable pageable = PageRequest.of(0, 12);
+		Page<ProductMinDTO> result = service.findAll(productName, pageable);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getSize(), 1);
+		Assertions.assertEquals(result.iterator().next().getName(), productName);
 		
 	}
 	
